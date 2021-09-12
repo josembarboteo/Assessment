d0d441d32bbb972cb0be6108fc5cde181c677b63
Jose Manuel Barboteo Company <josembarboteo@gmail.com>
2021-09-12 22:16:21 +0200

    [A01] Add test scenario for creating users

--- src/modules/users/create.user.dto.ts
+++ src/modules/users/create.user.dto.ts
@@ -1,4 +1,10 @@
 export class CreateUserDto {
+  constructor({ name, lastName, phone }) {
+    this.name = name;
+    this.lastName = lastName;
+    this.phone = phone;
+  }
+
   name: string;
   lastName: string;
   phone: string;
---
+++ test/unit/users/user.service.spec.ts
@@ -0,0 +1,37 @@
+import { getModelToken } from '@nestjs/mongoose';
+import { Test } from '@nestjs/testing';
+import { UserService } from 'src/modules/users/users.service';
+import { UserDataMotherObject } from './userData.mootherObject';
+
+describe('User service test', () => {
+  let userService: UserService;
+
+  beforeEach(async () => {
+    function mockUserModel(dto: any) {
+      this.data = dto;
+      this.save = () => {
+        return this.data;
+      };
+    }
+
+    const module = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getModelToken('User'),
+          useValue: mockUserModel,
+        },
+      ],
+    }).compile();
+
+    userService = module.get<UserService>(UserService);
+  });
+
+  it('Shoould create user correctly', async () => {
+    const userDto = UserDataMotherObject.getOne();
+    const user = await userService.createUser(userDto);
+    expect(user.name).toBe(userDto.name);
+    expect(user.lastName).toBe(userDto.lastName);
+    expect(user.phone).toBe(userDto.phone);
+  });
+});
