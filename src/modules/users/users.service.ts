4a9f0a8c945fb17338e3252596d6e484a53303f5
Jose Manuel Barboteo Company <josembarboteo@gmail.com>
2021-09-13 00:19:15 +0200

    [A02] Updatee persistance on users
    This is to avoid duplicity while creating new users

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -9,8 +9,11 @@ export class UserService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
   async createUser(inputModel: CreateUserDto): Promise<User> {
-    const user = new this.userModel(inputModel);
-    return user.save();
+    return this.userModel
+      .findOneAndUpdate({ phone: inputModel.phone }, inputModel, {
+        upsert: true,
+      })
+      .exec();
   }
 
   async findAll(): Promise<User[]> {
